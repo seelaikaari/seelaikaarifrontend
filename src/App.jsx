239c67628b5f24ba8345579f31864c9e0417ca8c
krishnamoorthy3 <krishnamoorthym3009@gmail.com>
2025-02-02 21:23:43 +0530

    Navbar design and component and PAges updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+//Components
+import NavBar from "./Components/NavBar/Navbar"
+import Home from "./Pages/Home/Home"
+import About from "./Pages/About/About"
+import Contact from "./Pages/Contact/Contact"
+import Product from "./Pages/Product/Product"
+import { Route,Routes } from "react-router-dom"; 
+//Css
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>hellow</h1>
+      <NavBar/>
+
+      <main>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/About" element={<About/>} />
+            <Route path="/Contact" element={<Contact/>} />
+            <Route path="/Product" element={<Product/>} />
+          </Routes>
+      </main>
+
+
     </>
   )
 }
