3ad632f7d8b691a80e6ded0e5edc44b0fa7388c1
Akilesh-A <akileshanand21@gmail.com>
2025-02-03 19:02:33 +0530

    updated  contact us pages

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
     e.preventDefault();
     if (validateForm()) {
       console.log('Form Data:', formData);
-      // You can send the form data to the server or perform other actions here
+     
     }
   };
 
@@ -71,7 +71,7 @@ const Contact = () => {
       <div className="map-container">
         <iframe 
           title="Location Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.905795154984!2d80.2171425147017!3d13.08064029079656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267960fbbebf3%3A0x6a56a20d2063a350!2sAB-149%2C%202nd%20Floor%20%2C%203rd%20Main%20Road%2C%20Anna%20Nagar%2C%20Chennai%2C%20Tamil%20Nadu%2C%20India!5e0!3m2!1sen!2sus!4v1675368374261!5m2!1sen!2sus" 
+          src="https://www.google.com/maps/place/Bengaluru,+Karnataka/@12.9539974,77.6309395,11z/data=!3m1!4b1!4m6!3m5!1s0x3bae1670c9b44e6d:0xf8dfc3e8517e4fe0!8m2!3d12.9715987!4d77.5945627!16zL20vMDljMTc?entry=ttu&g_ep=EgoyMDI1MDEyOS4xIKXMDSoASAFQAw%3D%3Dhttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.905795154984!2d80.2171425147017!3d13.08064029079656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5267960fbbebf3%3A0x6a56a20d2063a350!2sAB-149%2C%202nd%20Floor%20%2C%203rd%20Main%20Road%2C%20Anna%20Nagar%2C%20Chennai%2C%20Tamil%20Nadu%2C%20India!5e0!3m2!1sen!2sus!4v1675368374261!5m2!1sen!2sus" 
           width="100%" 
           height="300" 
           style={{border: 0}} 
@@ -80,17 +80,16 @@ const Contact = () => {
         </iframe>
       </div>
 
-      {/* Contact Info Section */}
+  
       <div className="contact-info">
         <h2>Contact Information</h2>
-        <p><strong>Email:</strong> contact@studio149fashion.com</p>
-        <p><strong>Phone Number:</strong> 044-43531466</p>
-        <p><strong>WhatsApp:</strong> +91 8148637647</p>
-        <p><strong>Address:</strong> AB-149, 2nd Floor, 3rd Main Road, Anna Nagar, Chennai-600040</p>
+        <p><strong>Email:</strong> customersupport@seelaikaari.com</p>
+        <p><strong>Phone Number:</strong> 044-123654789</p>
+        <p><strong>WhatsApp:</strong> +91 1234567895</p>
+        <p><strong>Address:</strong> AB-149, 2nd Floor, 3rd Main Road</p>
         <p><strong>Working Hours:</strong> 10:30 AM to 6:30 PM Mon-Sun</p>
       </div>
 
-      {/* Contact Form Section */}
       <form className="contact-form" onSubmit={handleSubmit}>
         <h3>Get in Touch</h3>
         <p className="intro-text">We'd love to hear from you about our entire service. Your comments and suggestions will be highly appreciated. Please complete the form below.</p>
