9a4f038c51052a2af74dc74aa4be9769628f4d0b
krishnamoorthy3 <krishnamoorthym3009@gmail.com>
2025-04-07 20:13:48 +0530

    details changed

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,5 +1,6 @@
 import "../About/About.css";
 import "animate.css";
+import aboutimg from "../../assets/images/banner-images/home-banner-1.jpg"
 
 const About = () => {
   return (
@@ -20,7 +21,7 @@ const About = () => {
 
           <h2 className="section-heading">Our Story</h2>
           <p>
-            Seelaikari was founded with a simple yet profound vision—to
+            Seelaikaari was founded with a simple yet profound vision—to
             reintroduce the charm of handwoven sarees to modern women. Inspired
             by generations of weaving expertise, we work closely with artisans
             from various regions to bring you sarees that embody craftsmanship,
@@ -45,8 +46,7 @@ const About = () => {
 
           <h2 className="section-heading">Our Craftsmanship</h2>
           <p>
-            From the regal <strong>Kanjivaram silks</strong> to the delicate{" "}
-            <strong>Chikankari cottons</strong>, each saree is a masterpiece. We
+            From the regal <strong>Kanjivaram silks</strong> to the delicate each saree is a masterpiece. We
             take pride in sourcing pure fabrics, natural dyes, and traditional
             weaving techniques passed down through generations.
           </p>
@@ -61,9 +61,9 @@ const About = () => {
 
           <h2 className="section-heading">Meet Our Founder</h2>
           <p>
-            <strong>Amrutha</strong>, the visionary behind Seelaikari, has
+            <strong>Amrutha    </strong>, the visionary behind Seelaikari, has
             always been captivated by the grace of sarees. With a deep-rooted
-            love for heritage textiles, Akilesh embarked on a journey to create
+            love for heritage textiles, Amrutha embarked on a journey to create
             a brand that uplifts weavers and makes handloom sarees accessible to
             all.
           </p>
@@ -71,8 +71,7 @@ const About = () => {
 
         <div className="about-image-container animate__animated animate__fadeInRight">
           <img
-            src="https://files.oaiusercontent.com/file-F5eSqDRHn6Xd1mhoqnXvMo?se=2025-02-03T11%3A35%3A19Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Db9f43d46-117e-4085-b0cd-c3255dcc3da2.webp&sig=E5f4fVmQDR8toJhSURoehofHMTppV5hz1ciEkM0dNrY%3D"
-            alt="Seelaikari Sarees"
+            src={aboutimg}
             className="about-image"
           />
         </div>
